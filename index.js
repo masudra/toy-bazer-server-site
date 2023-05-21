bd9d8c2c37800b13df3bb8ab70b2322bc4e01f91
masudra <masudrana6278312@gmail.com>
2023-05-22 01:32:31 +0600

    get data  using findone methode

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app =express();
 const port = process.env.PORT ||5000;
@@ -23,6 +23,26 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const  toysCollection = client.db('toyBazar').collection('allToysData');
+
+    app.get('/alltoysdata',async(req,res)=>{
+        const cursor = toysCollection.find();
+        const result = await cursor.toArray();
+        res.send(result)
+    })
+
+    app.get('/alltoysdata/:id',async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await toysCollection.findOne(query)
+      res.send(result)
+    })
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
